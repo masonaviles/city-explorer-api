6f7bade2c12b881d180090c0ed17caac30d44834
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-24 22:06:49 -0700

    server, attempt to add weather api

--- server.js
+++ server.js
@@ -1,65 +1,71 @@
 'use strict';
+// ===============================================
+// don't forget to do an npm install:
+// express, dotenv, cors, superagent
 
-// bring in the express libraray
-// don't forget to do an npm install express
+// bring in the express library
+// initalizing the express library so I can use it
 const express = require('express');
+const app = express();
 
 //allows us to access our env variables
 require('dotenv').config();
 
 //allow our front-end to access our server
-const cors = require('cors');
-
-// initalizing the express library so I can use it
-const app = express();
-
 //this allows anyone to access our server - aka - the worlds worst body guard
+const cors = require('cors');
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+// superagent is going to get us data
+const superagent = require('superagent');
 
+// ===============================================
 
-const myShoppingList = ['coffee', 'eggs', 'facial mask', 'aisan shrimp', 'energy drink', 'deordant'];
-//use weather data
-const weather = require('./data/weather.json');
-const weatherArray = weather.data;
- 
+const PORT = process.env.PORT || 3002;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+// ===============================================
+
+// proof of life
 app.get('/', function (request, response) {
   response.send('Hello World')
 });
 
-app.get('/shopping', (request, response) => {
-  response.send(myShoppingList);
-})
+//use weather data
+app.get('/weather', handleGetWeather);
 
-// hook up weather data
-// api endpoint
-app.get('/weather', function(request, response){
-  console.log(weather.lat);
-  const latitude = weather.lat;
-  const longitude = weather.lon;
-  const city = weather.city_name;
-  const forecastArray = weatherArray.map(element => {
-    return new Forecast(element.datetime, element.weather.description);
-  });
-
-  const cityData = {
-    latitude: latitude,
-    longitude: longitude,
-    city: city,
-    forecastArray: forecastArray
-  };
-
-  response.send(cityData);
-});
-
-// constructor function for a Forecast
-class Forecast {
-  constructor(date, description) {
-    this.date = date;
-    this.description = description;
-  }
+function handleGetWeather(req, res){
+  const cityName = req.query.search_query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${WEATHER_API_KEY}`;
+  superagent
+    .get(url)
+    .then(stuffThatComesBack => {
+      const output = stuffThatComesBack.body.data.map(day => new Weather(day));
+      res.send(output);
+    }).catch(errorThatComesBack => {
+      res.status(500).send(errorThatComesBack);
+    });
 }
+function Weather(data){
+  this.forecast = data.weather.description;
+  this.time = data.datetime;
+}
+
+// function handleWeather(request, response) {
+//   console.log('made it to weather');
+
+//   const forecastArray = weather.data.map(day => {
+//     return new Forecast(day, weather.city_name, weather.lat, weather.lon);
+//   });
+//   response.status(200).send(forecastArray);
+// }
+
+// function Forecast(obj, city, lat, lon) {
+//   this.desc = obj.weather.description;
+//   this.date = obj.datetime;
+//   this.city = city;
+//   this.lat = lat;
+//   this.lon = lon;
+// }
  
 
 // turn on the server
